3d197f85153b17087bfdd269ec43bbc61d5eed10
mixxamm <maximjanssensj5@gmail.com>
2020-01-02 15:26:03 +0100

    Voeg store toe

--- src/main.js
+++ src/main.js
@@ -4,14 +4,17 @@ import router from "./router";
 import vuetify from "./plugins/vuetify";
 import axios from "axios";
 import VueAxios from "vue-axios";
-import VueQrcode from '@chenfengyuan/vue-qrcode';
+import VueQrcode from "@chenfengyuan/vue-qrcode";
+import store from "./store";
 
 Vue.use(VueAxios, axios);
 Vue.component(VueQrcode.name, VueQrcode);
 Vue.config.productionTip = false;
 
+
 new Vue({
   router,
   vuetify,
+  store,
   render: h => h(App)
 }).$mount("#app");
