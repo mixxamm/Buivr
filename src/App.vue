adb736b00dd120c7996be6c8e0c7182aa49781a9
mixxamm <maximjanssensj5@gmail.com>
2020-01-10 11:29:37 +0100

    Maak bottom navigation bar

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,13 @@
     <div>
       <v-toolbar color="amber">
         <v-toolbar-title>Buivr</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-text class="body-1">{{naam}}</v-text>
+      </v-toolbar>
+      <router-view class="router"></router-view>
+    <NaamDialog />
+      <!-- <v-toolbar color="amber">
+        <v-toolbar-title>Buivr</v-toolbar-title>
 
         <v-spacer></v-spacer>
 
@@ -12,7 +19,7 @@
           <v-btn text to="/account">Account | {{ naam }}</v-btn>
         </v-toolbar-items>
 
-        <!-- <template v-if="$vuetify.breakpoint.smAndUp">
+         <template v-if="$vuetify.breakpoint.smAndUp">
           <v-btn icon>
             <v-icon>mdi-export-variant</v-icon>
           </v-btn>
@@ -22,11 +29,31 @@
           <v-btn icon>
             <v-icon>mdi-plus-circle</v-icon>
           </v-btn>
-        </template> -->
-      </v-toolbar>
+        </template> 
+      </v-toolbar> -->
+      <v-bottom-navigation
+    :value="activeBtn"
+    grow
+    color="amber"
+    fixed
+    shift
+  >
+    <v-btn to="/">
+      <span>Bibs</span>
+      <v-icon>mdi-library</v-icon>
+    </v-btn>
+
+    <v-btn to="/colleges">
+      <span>Colleges</span>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn to="/account">
+      <span>Account</span>
+      <v-icon>mdi-account-circle</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
     </div>
-    <router-view></router-view>
-    <NaamDialog />
   </v-app>
 </template>
 <script>
@@ -53,3 +80,8 @@ export default {
   }
 };
 </script>
+<style scoped>
+.router{
+  margin-bottom: 56px;
+}
+</style>
